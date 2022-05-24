da296ba4cbceb55f2e41713d8413d3388bd9b506
Kumar Parikshit <87370837+KumarParikshit@users.noreply.github.com>
2022-05-24 15:35:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -82,6 +82,7 @@
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="https://cpanel.acclaimhosting.ml">Login</a>
         <a class="nav-link fw-bold py-1 px-0 active" href="./signup.php">Sign Up</a>
+        <a class="nav-link fw-bold py-1 px-0 active" href="./faq.php">FAQs</a>
       </nav>
     </div>
   </header>
