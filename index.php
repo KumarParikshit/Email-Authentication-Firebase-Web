0cfcaca151d6f5b6170fb917c9e431b6d03c219f
Kumar Parikshit <87370837+KumarParikshit@users.noreply.github.com>
2022-05-24 14:06:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.98.0">
+    <title>AcclaimHosting - Free Web Hosting</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/cover/">
+
+    
+
+    
+
+<link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, 0.522);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="./css/cover.css" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">AcclaimHosting</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="https://cpanel.acclaimhosting.ml">Login</a>
+        <a class="nav-link fw-bold py-1 px-0 active" href="./signup.php">Sign Up</a>
+      </nav>
+    </div>
+  </header>
+  <br>
+
+  <main class="px-3">
+    <h1>AcclaimHosting - A Free Web Hosting Provider</h1>
+    <br>
+    <h4>Free Hosting</h4>
+    <h6>Sign up and get benefit of these features & more!</h6>
+    <br>
+    <p class="lead">10GB Cloud Storage & 100GB Bandwidth</p>
+    <p class="lead">Host Up To 10 Websites On Each Account</p>
+    <p class="lead">10 MySQL Databases, FTP & Email accounts</p>
+    <p class="lead">24/7 Monitoring & Firewall Protection</p>
+    <p class="lead">Free Website Builder & Softaculous</p>
+    <p class="lead">vPanel Easy-To-Use Control Panel</p>
+  </main>
+   <br>
+   <h1>To get started click Sign Up button which is above now!!</h1>
+   <br>
+   <br>
+  <footer class="mt-auto text-white-50">
+    <p>Copyright @<a href="https://acclaimhosting.tk/" class="text-white">AcclaimHosting 2022</a></p>
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
